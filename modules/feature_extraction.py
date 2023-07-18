c62b1f680ef305bca87e50960926b6d2e2f7c568
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-07-18 09:46:49 -0400

    increased 4 layer CNN width

--- modules/feature_extraction.py
+++ modules/feature_extraction.py
@@ -44,25 +44,25 @@ class VGG_FeatureExtractor_4(nn.Module):
 
         self.ConvNet = nn.Sequential( # 1 x 20 x 100
             # the first conv layer
-            nn.Conv2d(input_channel, self.output_channel[0], kernel_size=3, stride=1, padding=1),  # 8 x 20 x 100
+            nn.Conv2d(input_channel, self.output_channel[1], kernel_size=3, stride=1, padding=1),  # 8 x 20 x 100
             nn.MaxPool2d(kernel_size=2, stride=2),  # 8 x 10 x 50
             nn.ReLU(True),
             nn.Dropout(p=dropout_rate),
 
             # the second conv layer
-            nn.Conv2d(self.output_channel[0], self.output_channel[1], kernel_size=3, stride=1, padding=1),  # 16 x 10 x 50
+            nn.Conv2d(self.output_channel[1], self.output_channel[2], kernel_size=3, stride=1, padding=1),  # 16 x 10 x 50
             nn.MaxPool2d(kernel_size=2, stride=2),  # 16 x 5 x 25
             nn.ReLU(True),
             nn.BatchNorm2d(self.output_channel[1]),
 
             # the third conv layer
-            nn.Conv2d(self.output_channel[1], self.output_channel[2], kernel_size=3, stride=1, padding=1),  # 32 x 5 x 25
+            nn.Conv2d(self.output_channel[2], self.output_channel[3], kernel_size=3, stride=1, padding=1),  # 32 x 5 x 25
             nn.MaxPool2d(kernel_size=2, stride=2),  # 32 x 2 x 12
             nn.ReLU(True),
             nn.Dropout(p=dropout_rate),
 
             # the fourth conv layer
-            nn.Conv2d(self.output_channel[2], self.output_channel[3], kernel_size=3, stride=1, padding=1),  # 64 x 2 x 12
+            nn.Conv2d(self.output_channel[3], self.output_channel[3], kernel_size=3, stride=1, padding=1),  # 64 x 2 x 12
             nn.MaxPool2d(kernel_size=2, stride=2),  # 64 x 1 x 6
             nn.ReLU(True),
             nn.Dropout(p=dropout_rate),
